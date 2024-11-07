1d76a12f2b57cc56a6d2d2134c001c8c6478db84
John Flanagan <jflann9@icloud.com>
2024-11-07 12:33:09 -0600

    added automated tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# pages/tests.py
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class PageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        #get request to get status_code 200, which is a successful status code (ex. 404 file not found)
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
